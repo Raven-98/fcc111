8bf456b26fc87b691d8c35a7af85f7d4bdb34504
Raven-98 <56961368+Raven-98@users.noreply.github.com>
2021-06-16 07:19:04 +0300

    Update fcc111.py

--- fcc111.py
+++ fcc111.py
@@ -2,9 +2,6 @@
 
 import numpy as np
 
-def toFixed(numObj, digits=0):
-    return float(f"{numObj:.{digits}f}")
-
 def rotate(r, phi):
     M = np.matrix([[np.cos(np.radians(phi)), -np.sin(np.radians(phi))],
                    [np.sin(np.radians(phi)),  np.cos(np.radians(phi))]])
@@ -23,9 +20,9 @@ for n in range(1, 101):
         r = iA * r
         k = True
         for i in range(2):
-            if toFixed(r.item(i), 3) % 1 < 0.999 and toFixed(r.item(i), 3) % 1 > 0.001:
+            if (r.item(i) % 1 < 0.9985 and r.item(i) % 1 > 0.0015):
             #if toFixed(r.item(i), 2) % 1 != 0:
                 k = False
                 
         if k == True:
-            print(n, '\t', toFixed(phi, 2), '\t[', toFixed(r.item(0), 3), ',', toFixed(r.item(1), 3), ']')
+            print(n, '\t', phi, '\t[', r.item(0), ',', r.item(1), ']')
